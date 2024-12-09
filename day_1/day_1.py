4673616d26b9f8336ab0d8167a8e4b5e090636d5
Naomi <naomi.christie@ibusinessfunding.com>
2024-12-09 15:14:57 +0000

    Day 1, part 2

--- day_1/day_1.py
+++ day_1/day_1.py
@@ -1,4 +1,4 @@
-def main():
+def part_1():
     # Throughout the Chief's office, the historically significant locations are listed not by name but by a unique number called the location ID. To make sure they don't miss anything, The Historians split into two groups, each searching the office and trying to create their own complete list of location IDs.
 
     # There's just one problem: by holding the two lists up side by side (your puzzle input), it quickly becomes clear that the lists aren't very similar. Maybe you can help The Historians reconcile their lists?
@@ -31,9 +31,37 @@ def main():
     list_b_ordered = order_list(list_b)
     distances = get_distances_between_two_lists(list_a_ordered, list_b_ordered)
     total_distance = sum_list(distances)
-    print(total_distance)
+    print(f"total_distance: {total_distance}")
     return total_distance
 
+def part_2():
+    # This time, you'll need to figure out exactly how often each number from the left list appears in the right list. Calculate a total similarity score by adding up each number in the left list after multiplying it by the number of times that number appears in the right list.
+
+    # Here are the same example lists again:
+
+    # 3   4
+    # 4   3
+    # 2   5
+    # 1   3
+    # 3   9
+    # 3   3
+    # For these example lists, here is the process of finding the similarity score:
+
+    # The first number in the left list is 3. It appears in the right list three times, so the similarity score increases by 3 * 3 = 9.
+    # The second number in the left list is 4. It appears in the right list once, so the similarity score increases by 4 * 1 = 4.
+    # The third number in the left list is 2. It does not appear in the right list, so the similarity score does not increase (2 * 0 = 0).
+    # The fourth number, 1, also does not appear in the right list.
+    # The fifth number, 3, appears in the right list three times; the similarity score increases by 9.
+    # The last number, 3, appears in the right list three times; the similarity score again increases by 9.
+    # So, for these example lists, the similarity score at the end of this process is 31 (9 + 4 + 0 + 0 + 9 + 9).
+
+    # Once again consider your left and right lists. What is their similarity score?
+
+    list_a, list_b = extract_lists_from_txt("day_1_inputs.txt")
+    set_a = set(order_list(list_a))
+    similarity = count_freq(set_a, list_b)
+    print(f"similarity: {similarity}")
+    return similarity
 
 def extract_lists_from_txt(filepath):
     list_a = []
@@ -62,6 +90,14 @@ def get_distances_between_two_lists(list_a_ordered, list_b_ordered):
 def sum_list(my_list):
     return sum(my_list)
 
+def count_freq(set_a, list_b):
+    count = 0
+    for item in set_a:
+        for jitem in list_b:
+            if item == jitem:
+                count += jitem
+    return count
 
 if __name__ == "__main__":
-    main()
+    part_1()
+    part_2()
