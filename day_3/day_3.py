4a801d262e39d3f9cbb7fdfb50fb87a0ccb38fc3
Naomi <naomi.christie@ibusinessfunding.com>
2024-12-10 13:17:09 +0000

    Day 3, part 2

--- day_3/day_3.py
+++ day_3/day_3.py
@@ -10,13 +10,35 @@ def part_1():
     print(output)
     return output
 
+def part_2():
+    filename = "day_3_input.txt"
+    input_text = get_input_text(filename)
+    relevant_text = extract_relevant_text_part_2(input_text)
+
+    output = 0
+    perform = True
+    for line in relevant_text:
+        if line == "do()":
+            perform = True
+        elif line == "don't()":
+            perform = False
+        else:
+            if perform:
+                output += eval(line)
+    print(output)
+    return output
+
 def get_input_text(filename):
     with open(filename, "r") as file:
         return file.read()
 
-
 def extract_relevant_text(input_text):
     pattern = r'mul\(\d+,\d+\)'
+    relevant_text = re.findall(pattern, input_text)
+    return relevant_text
+
+def extract_relevant_text_part_2(input_text):
+    pattern = r'mul\(\d+,\d+\)|do\(\)|don\'t\(\)'
     relevant_text = re.findall(pattern, input_text)
     return relevant_text
 
@@ -24,5 +46,6 @@ def mul(int_a, int_b):
     return int_a * int_b
 
 if __name__ == "__main__":
-    part_1()
+    # part_1()
+    part_2()
 
