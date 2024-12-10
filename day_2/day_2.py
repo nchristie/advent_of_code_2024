0a8eeced1d9f982064dba92a35c7461226169095
Naomi <naomi.christie@ibusinessfunding.com>
2024-12-10 12:28:46 +0000

    Day 2, part 2 - incomplete

--- day_2/day_2.py
+++ day_2/day_2.py
@@ -34,8 +34,41 @@ def part_1():
     print(f"safe_reports: {safe_reports}")
     return safe_reports
 
+
 def part_2():
-    return
+    """
+    The engineers are surprised by the low number of safe reports until they realize they forgot to tell you about the Problem Dampener.
+
+    The Problem Dampener is a reactor-mounted module that lets the reactor safety systems tolerate a single bad level in what would otherwise be a safe report. It's like the bad level never happened!
+
+    Now, the same rules apply as before, except if removing a single level from an unsafe report would make it safe, the report instead counts as safe.
+
+    More of the above example's reports are now safe:
+
+    7 6 4 2 1: Safe without removing any level.
+    1 2 7 8 9: Unsafe regardless of which level is removed.
+    9 7 6 2 1: Unsafe regardless of which level is removed.
+    1 3 2 4 5: Safe by removing the second level, 3.
+    8 6 4 4 1: Safe by removing the third level, 4.
+    1 3 6 7 9: Safe without removing any level.
+    Thanks to the Problem Dampener, 4 reports are actually safe!
+
+    Update your analysis by handling situations where the Problem Dampener can remove a single level from unsafe reports. How many reports are now safe?
+    """
+    part_2_safe_reports = 0
+    all_reports = extract_reports_from_txt("day_2_inputs.txt")
+
+    for report in all_reports:
+        clean_report = clean_up(report)
+        report_check = safe_report(clean_report)
+        if report_check == []:
+            part_2_safe_reports += 1 
+        else:
+            pass
+            # print(f"report: {report}, clean_report: {clean_report}, report_check: {report_check}")
+    print(f"part_2_safe_reports: {part_2_safe_reports}")
+    return part_2_safe_reports
+
 
 def extract_reports_from_txt(filepath):
     all_reports = []
@@ -45,37 +78,82 @@ def extract_reports_from_txt(filepath):
             all_reports.append([int(x) for x in report])
     return all_reports
 
+
 def safe_report(report):
     rising = 0
     falling = 0
-    for i in range(len(report)-1):
+
+    errors = []
+    for i in range(len(report) - 1):
         last_level = report[i]
-        current_level = report[i+1]
+        current_level = report[i + 1]
+
         if levels_match(last_level, current_level):
-            return False
+            errors.append(f"levels_match")
         if step_size_unsafe(last_level, current_level):
-            return False
+            errors.append(f"step_size_unsafe")
         if level_rising(last_level, current_level):
-            if falling > 0:
-                return False
             rising += 1
         else:
-            if rising > 0:
-                return False
             falling += 1
+
+        if rising == 1 and falling == 1:
+            errors.append("fall_rise")
+
+    return errors
+
+
+def clean_up(report):
+    report_len = len(report)
+    first_index_in_report = 0
+    last_index_in_report = report_len - 1
+    penultimate_index = report_len - 2
+    one_before_penultimate_index = report_len - 3
+    for i in range(last_index_in_report):
+        level_1 = report[i]
+        level_2 = report[i + 1]
+    
+        # check if levels rise and fall
+        if i <= one_before_penultimate_index:
+            level_3 = report[i + 2]
+            if do_levels_rise_and_fall(level_1, level_2, level_3):
+                del report[i + 1]
+                break
+
+        if levels_match(level_1, level_2):
+            del report[i]
+            break
+
+        if step_size_unsafe(level_1, level_2):
+            if i == first_index_in_report:
+                del report[first_index_in_report]
+            if i == penultimate_index:
+                del report[last_index_in_report]
+            break
+
+    return report
+
+def do_levels_rise_and_fall(level_1, level_2, level_3):
+    if level_1 >= level_2 >= level_3:
+        return False
+    if level_1 <= level_2 <= level_3:
+        return False
     return True
-        
+
 
 def levels_match(last_level, current_level):
     return last_level == current_level
 
+
 def level_rising(last_level, current_level):
     return current_level > last_level
+
 
 def step_size_unsafe(last_level, current_level):
     if abs(current_level - last_level) > 3:
         return True
     return False
+
 
 if __name__ == "__main__":
     part_1()
--- day_2/test_day_2.py
+++ day_2/test_day_2.py
@@ -6,46 +6,171 @@ import pytest
 # WHEN
 # THEN
 
+
+@pytest.mark.parametrize(
+    "all_reports, expected",
+    [
+        (
+            [
+                [7, 6, 4, 2, 1],
+                [1, 2, 7, 8, 9],
+                [9, 7, 6, 2, 1],
+                [1, 3, 2, 4, 5],
+                [8, 6, 4, 4, 1],
+                [1, 3, 6, 7, 9],
+            ],
+            4,
+        ),
+        # (
+        #     [
+        #         [35, 37, 38, 41, 43, 41],
+        #         [64, 66, 69, 71, 72, 72],
+        #         [45, 47, 50, 51, 52, 53, 55, 59],
+        #         [16, 18, 19, 20, 23, 29],
+        #         [36, 39, 41, 43, 44, 41, 44],
+        #         [42, 45, 46, 44, 42],
+        #         [82, 85, 86, 87, 88, 86, 86],
+        #         [42, 45, 46, 45, 47, 51],
+        #         [57, 60, 62, 65, 63, 65, 70],
+        #         [88, 90, 90, 93, 95, 97],
+        #     ],
+        #     5
+        # )
+    ],
+)
 @patch("day_2.extract_reports_from_txt")
-def test_part_1(mock_extract):
+def test_part_1(mock_extract, all_reports, expected):
     # GIVEN
-    all_reports = [
-        [7, 6, 4, 2, 1],
-        [1, 2, 7, 8, 9],
-        [9, 7, 6, 2, 1],
-        [1, 3, 2, 4, 5],
-        [8, 6, 4, 4, 1],
-        [1, 3, 6, 7, 9],
-    ]
     mock_extract.return_value = all_reports
-    
+
     # WHEN
-    expected = 2
     actual = day_2.part_1()
-    
+
     # THEN
     assert expected == actual
 
-@pytest.mark.parametrize("report, expected", [
-    ([7, 6, 4, 2, 1], True),
-    ([1, 2, 7, 8, 9], False),
-    ([9, 7, 6, 2, 1], False),
-    ([1, 3, 2, 4, 5], False),
-    ([8, 6, 4, 4, 1], False),
-    ([1, 3, 6, 7, 9], True),
-])
-def test_safe_report(report, expected):   
+
+@pytest.mark.parametrize(
+    "report, expected",
+    [
+        ([7, 6, 4, 2, 1], True),
+        ([1, 2, 7, 8, 9], False),
+        ([9, 7, 6, 2, 1], False),
+        ([1, 3, 2, 4, 5], False),
+        ([8, 6, 4, 4, 1], False),
+        ([1, 3, 6, 7, 9], True),
+    ],
+)
+def test_safe_report(report, expected):
     # WHEN
     actual = day_2.safe_report(report)
-    
+
     # THEN
     assert expected == actual
 
-@pytest.mark.xfail
-def test_part_2():
-    # GIVEN
+
+@pytest.mark.parametrize(
+    "report, expected",
+    [
+        ([7, 6, 4, 2, 1], []),
+        ([1, 2, 7, 8, 9], ["step_size_unsafe"]),
+        ([9, 7, 6, 2, 1], ["step_size_unsafe"]),
+        ([1, 3, 2, 4, 5],["fall_rise"]),
+        ([8, 6, 4, 4, 1], ["levels_match"]),
+        ([1, 3, 6, 7, 9], []),
+    ],
+)
+def test_safe_report(report, expected):
     # WHEN
-    expected = True
-    actual = False
+    actual = day_2.safe_report(report)
+
+    # THEN
+    assert expected == actual
+
+
+@patch("day_2.extract_reports_from_txt")
+def test_part_2(mock_extract):
+    # GIVEN
+    all_reports = [
+        [7, 6, 4, 2, 1], # safe
+        [1, 2, 7, 8, 9], # unsafe
+        [9, 7, 6, 2, 1], # unsafe
+        [1, 3, 2, 4, 5], # safe
+        [8, 6, 4, 4, 1], # safe
+        [1, 3, 6, 7, 9], # safe
+    ] 
+    mock_extract.return_value = all_reports
+
+    # WHEN
+    expected = 4
+    actual = day_2.part_2()
+
+    # THEN
+    assert expected == actual
+
+
+@patch("day_2.extract_reports_from_txt")
+def test_part_2_example(mock_extract):
+    # GIVEN
+    all_reports = [
+        [69, 67, 64, 63, 59], # safe
+    ] 
+    mock_extract.return_value = all_reports
+
+    # WHEN
+    expected = 0
+    actual = day_2.part_2()
+
+    # THEN
+    assert expected == actual
+
+@pytest.mark.parametrize(
+    "levels, expected",
+    [
+        ([7, 6, 4], False),
+        ([1, 3, 2], True),
+        ([3, 1, 2], True),
+        ([1, 1, 2], False),
+        ([1, 2, 2], False),
+    ],
+)
+def test_do_levels_rise_and_fall(levels, expected):
+    # WHEN
+    actual = day_2.do_levels_rise_and_fall(levels[0], levels[1], levels[2])
+
+    # THEN
+    assert expected == actual
+
+
+@pytest.mark.parametrize(
+    "report, expected",
+    [
+        ([1, 1, 2, 3, 4], [1, 2, 3, 4]),
+        ([1, 5, 2, 3, 4], [1, 2, 3, 4]),
+        ([1, 8, 2, 3, 4], [1, 2, 3, 4]),
+        ([9, 1, 8, 7, 6], [9, 8, 7, 6]),
+        ([9, 1, 8, 7, 6, 8], [9, 8, 7, 6, 8]),
+        ([8, 6, 4, 4, 1], [8, 6, 4, 1]),
+        ([69, 67, 64, 63, 59], [69, 67, 64, 63])
+    ],
+)
+def test_clean_up(report, expected):
+    # WHEN
+    actual = day_2.clean_up(report)
+
+    # THEN
+    assert expected == actual
+
+@pytest.mark.parametrize(
+    "last_level, current_level, expected",
+    [
+        (63, 59, True)
+    ]
+
+)
+def test_step_size_unsafe(last_level, current_level, expected):
+    # WHEN
+    actual = day_2.step_size_unsafe(last_level, current_level)
+
     # THEN
     assert expected == actual
