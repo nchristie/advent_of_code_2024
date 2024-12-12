c929cd694286d7da35d8aaa2cea0235feb89b927
Naomi <naomi.christie@ibusinessfunding.com>
2024-12-12 11:14:37 +0000

    Day 11, part 2, incomplete

--- day_11/day_11_part_2.py
+++ day_11/day_11_part_2.py
@@ -1,39 +1,41 @@
 import time
 
+NUMBER_OF_ITERATIONS = 25
+
 lookup_table = {}
-output_table = {
-    112: 1, 
-    1110: 1, 
-    163902: 1, 
-    0: 1, 
-    7656027: 1, 
-    83039: 1, 
-    9: 1, 
-    74: 1
-}
 
-def part_1():
-    input = [112, 1110, 163902, 0, 7656027, 83039, 9, 74]
+def part_2():
+    output_table = {
+        112: 1, 
+        1110: 1, 
+        163902: 1, 
+        0: 1, 
+        7656027: 1, 
+        83039: 1, 
+        9: 1, 
+        74: 1
+    }
     rules = [rule_1, rule_2, rule_3]
-    output = input
-    len_output = len(output)
-    input_table = output_table
 
-    number_of_iterations = 1
-    for i in range(number_of_iterations):
+    for i in range(NUMBER_OF_ITERATIONS):
         start_time = time.time()
         print(f"\niteration: {i+1}")
-        output_table = run_rules(input_table, rules)
-        len_output = len(output)
-        print(f"len_output: {len_output}")
+        new_output_table = run_rules(rules, output_table)
+        print(f"len_output: {get_output_len(new_output_table)}")
         end_time = time.time()
         print("--- %s seconds ---" % round((end_time - start_time), 2))
-        print(output_table)
-        input_table = output_table
+        print(new_output_table)
+        output_table = new_output_table
 
-def run_rules(input_table, rules):
+def get_output_len(output_table):
+    len_output = 0
+    for k, v in output_table.items():
+        len_output += v
+    return len_output
+
+def run_rules(rules, output_table):
     new_output_table = {}
-    for input_item, number_of_instances in input_table.iteritems():
+    for input_item, number_of_instances in output_table.items():
         if lookup_table.get(input_item):
             morph = lookup_table[input_item]
         else:
@@ -44,9 +46,9 @@ def run_rules(input_table, rules):
                     break
         for item in morph:
             if new_output_table.get(item):
-                new_output_table[item] *= number_of_instances
+                new_output_table[item] += 1
             else:
-                new_output_table[item] = number_of_instances
+                new_output_table[item] = 1
 
     return new_output_table
 
@@ -69,6 +71,5 @@ def rule_3(input_item):
     # If none of the other rules apply, the stone is replaced by a new stone; the old stone's number multiplied by 2024 is engraved on the new stone.
     return [input_item * 2024]
 
-
 if __name__ == "__main__":
-    part_1()
+    part_2()
--- day_11/test_day_11.py
+++ day_11/test_day_11.py
@@ -1,27 +1,7 @@
 import day_11
+import day_11_part_2
 import pytest
-
-# Initial arrangement:
-# 125 17
-
-# After 1 blink:
-# 253000 1 7
-
-# After 2 blinks:
-# 253 0 2024 14168
-
-# After 3 blinks:
-# 512072 1 20 24 28676032
-
-# After 4 blinks:
-# 512 72 2024 2 0 2 4 2867 6032
-
-# After 5 blinks:
-# 1036288 7 2 20 24 4048 1 4048 8096 28 67 60 32
-
-# After 6 blinks:
-# 2097446912 14168 4048 2 0 2 4 40 48 2024 40 48 80 96 2 8 6 7 6 0 3 2
-
+RULES = [day_11_part_2.rule_1, day_11_part_2.rule_2, day_11_part_2.rule_3]
 
 @pytest.mark.parametrize(
     "input_item, expected",
@@ -33,3 +13,33 @@ def test_rule_2(input_item, expected):
 
     # THEN
     assert expected == actual
+
+def test_run_rules():
+    # GIVEN
+    rules = RULES
+    output_table = {
+        0: 1,
+        1: 1,
+        10: 1,
+        100: 1,
+        1000: 1
+    }
+
+    # WHEN
+    expected = {
+        0: 2,
+        1: 2,
+        10: 1,
+        2024: 1,
+        202400: 1,
+    }
+    actual = day_11_part_2.run_rules(rules, output_table)
+    
+    # THEN
+    assert expected == actual
+
+def test_get_output_len():
+    # GIVEN
+    # WHEN
+    # THEN
+    pass
